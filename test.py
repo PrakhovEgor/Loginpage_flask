83c1d4ca6c63baf8070b4ccda41c513c1582e48b
PrakhovEgor <prakhov.egor@gmail.com>
2022-03-21 18:12:12 +0300

    1
    1

---
+++ test.py
@@ -0,0 +1,6 @@
+from requests import get, post, delete
+
+print(delete('http://localhost:5000/api/news/999').json())
+# новости с id = 999 нет в базе
+
+print(delete('http://localhost:5000/api/news/1').json())
